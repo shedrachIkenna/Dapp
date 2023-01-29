f77e695cf26c7e4b00973b3c76221046840db852
shedrachIkenna <shedrach686@gmail.com>
2023-01-29 17:33:11 +0100

    JustTesting functions

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,4 +3,8 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     receive() external payable {}
+    function addFunds() external payable{}
+    function justTesting() external pure returns(uint){
+        return 2 + 2;
+    }
 }
