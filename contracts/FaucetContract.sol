c7cccaf0db05c6198ec8b9c4be0f7f65910087b4
shedrachIkenna <shedrach686@gmail.com>
2023-02-01 16:50:26 +0100

    Prevent duplication

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,28 +4,18 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
     uint public numOfFunders;
-    mapping(uint256 => address) private funders;
+    mapping(address => bool) private funders;
 
     receive() external payable {}
 
     function addFunds() external payable{
-        uint index = numOfFunders++;
-        funders[index] = msg.sender;
-    }
-
-    function getFundersAtIndex(uint8 index) external view returns(address){
-        return funders[index];
-    }
-
-    function getAllFunders() external view returns(address[] memory){
-        address[] memory _funders = new address[](numOfFunders);
-
-        for(uint i = 0; i < numOfFunders; i++) {
-            _funders[i] = funders[i];
+        address funder = msg.sender;
+        if (!funders[funder]) {
+            numOfFunders++;
+            funders[funder] = true;
         }
-
-        return _funders;
     }
+
 }
 
 //instance.addFunds({from: accounts[0], values:"2"})
