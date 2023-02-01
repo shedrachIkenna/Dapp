e272f4fb84cdc4cfbe71ef06d4465847c50ea9b7
shedrachIkenna <shedrach686@gmail.com>
2023-02-01 17:52:06 +0100

    Rewrote all functions to avoid duplications

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -5,17 +5,37 @@ contract Faucet {
 
     uint public numOfFunders;
     mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
 
     receive() external payable {}
 
     function addFunds() external payable{
         address funder = msg.sender;
-        if (!funders[funder]) {
-            numOfFunders++;
+        if (!funders[funder]) { 
+            uint index = numOfFunders++; // You can create a variable which is initialised to zero before its incremented to acheive the same result
             funders[funder] = true;
+            lutFunders[index] = funder;
+            // numOfFunders++; // Incrementing numOfFunders at the bottom of this block of code means the address of a funder is stored at index[0] before the increment happens 
         }
     }
 
+    function getFunderAtIndex(uint8 index) external view returns(address){
+        return lutFunders[index];
+    }
+
+    function getAllFunders() external view returns(address[] memory){
+        address[] memory _funders = new address[](numOfFunders);
+
+        for(uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
+    }
 }
 
-//instance.addFunds({from: accounts[0], values:"2"})
+// const instance = await Faucet.deployed()
+// instance.addFunds({from: accounts[0], values:"200000000"})
+// instance.addFunds({from: accounts[1], values:"200000000"})
+// instance.getFunderAtIndex(0)
+// instance.getAllFunders()
