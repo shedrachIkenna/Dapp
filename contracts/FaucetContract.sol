8efb7b78db93e602fffd3eff1193ac52a80a0a2e
shedrachIkenna <shedrach686@gmail.com>
2023-02-01 16:09:24 +0100

    numOfFunders example

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,21 +3,18 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-    address[] public funders;
+    uint public numOfFunders;
+    mapping(uint256 => address) private funders;
 
     receive() external payable {}
 
     function addFunds() external payable{
-        funders.push(msg.sender);
-    }
-
-    function getAllFunders() public view returns(address[] memory){
-        return funders;
+        uint index = numOfFunders++;
+        funders[index] = msg.sender;
     }
 
     function getFundersAtIndex(uint8 index) external view returns(address){
-        address[] memory _funders = getAllFunders();
-        return _funders[index];
+        return funders[index];
     }
 }
 
