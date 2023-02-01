900497c3992035e7c005969f02694336145c7e94
shedrachIkenna <shedrach686@gmail.com>
2023-02-01 16:37:23 +0100

    getAllFunders in the mapping list

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -16,6 +16,16 @@ contract Faucet {
     function getFundersAtIndex(uint8 index) external view returns(address){
         return funders[index];
     }
+
+    function getAllFunders() external view returns(address[] memory){
+        address[] memory _funders = new address[](numOfFunders);
+
+        for(uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+
+        return _funders;
+    }
 }
 
 //instance.addFunds({from: accounts[0], values:"2"})
