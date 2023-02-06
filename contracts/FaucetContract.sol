c8d96ea49027d058d888cf195876e14c056be5d1
shedrachIkenna <shedrach686@gmail.com>
2023-02-06 02:54:18 +0100

    Added IFaucet interface

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "./Owned.sol";
 import "./Logger.sol";
+import "./IFaucet.sol";
 
-contract Faucet is Owned, Logger{
+contract Faucet is Owned, Logger, IFaucet{
 
     uint public numOfFunders;
 
@@ -26,7 +27,7 @@ contract Faucet is Owned, Logger{
         return "Hello World";
     }
 
-    function addFunds() external payable{
+    function addFunds() override external payable{
         address funder = msg.sender;
         if (!funders[funder]) { 
             uint index = numOfFunders++; // You can create a variable which is initialised to zero before its incremented to acheive the same result
@@ -44,7 +45,7 @@ contract Faucet is Owned, Logger{
         //does something
     }
 
-    function withdraw(uint withdrawAmount) external limitWithdraw (withdrawAmount) {
+    function withdraw(uint withdrawAmount) override external limitWithdraw (withdrawAmount) {
         payable(msg.sender).transfer(withdrawAmount);
     }
 
@@ -64,8 +65,8 @@ contract Faucet is Owned, Logger{
 }
 
 // const instance = await Faucet.deployed()
-// instance.addFunds({from: accounts[0], values:"2000000000000000000"})
-// instance.addFunds({from: accounts[1], values:"2000000000000000000"})
-// instance.withdraw("500000000000000000", {from: account[1]})
+// instance.addFunds({from: accounts[0], value:"1000000000000000000"})
+// instance.addFunds({from: accounts[1], value:"2000000000000000000"})
+// instance.withdraw("500000000000000000", {from: accounts[1]})
 // instance.getFunderAtIndex(0)
 // instance.getAllFunders()
